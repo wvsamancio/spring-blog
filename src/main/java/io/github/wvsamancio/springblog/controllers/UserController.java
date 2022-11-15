5a56589870db305ed98080eb735e50d10ec4f002
Willian Amancio <wvsamancio@gmail.com>
2022-11-15 10:24:23 -0300

    added put method

--- src/main/java/io/github/wvsamancio/springblog/controllers/UserController.java
+++ src/main/java/io/github/wvsamancio/springblog/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -52,6 +53,20 @@ public class UserController {
         if (user.isPresent()) {
             repository.delete(user.get());
             return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<User> updatePasswd(@PathVariable Long id, @RequestBody User newUser) {
+        Optional<User> oldUser = repository.findById(id);
+        if (oldUser.isPresent()) {
+            User user = oldUser.get();
+            user.setId(id);
+            user.setPasswd(newUser.getPasswd());
+            repository.save(user);
+            return ResponseEntity.ok(user);
         }
         return ResponseEntity.notFound().build();
     }
--- src/main/java/io/github/wvsamancio/springblog/entities/User.java
+++ src/main/java/io/github/wvsamancio/springblog/entities/User.java
@@ -8,9 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @NoArgsConstructor
